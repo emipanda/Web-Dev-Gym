f9777f1079374fcd446ec14fec416aae1e9a82db
ytal <ytalamsel@gmail.com>
2021-09-18 10:14:58 +0300

    18.9 morning session

---
+++ Web Dev Proj Gym/Migrations/20210918070951_18.9morning.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web_Dev_Proj_Gym.Migrations
+{
+    public partial class _189morning : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admin",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AdminUserName = table.Column<int>(type: "int", nullable: false),
+                    AdminPassword = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AdminName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ManagersBranchID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admin", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Admin_Branch_ManagersBranchID",
+                        column: x => x.ManagersBranchID,
+                        principalTable: "Branch",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Admin_ManagersBranchID",
+                table: "Admin",
+                column: "ManagersBranchID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admin");
+        }
+    }
+}
